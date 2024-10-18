adb4d43904e258819cf75abb1cda7e98e3f861a4
beatrizgpfernandes <beatriz.fernandes@sptech.school>
2024-10-18 16:52:15 -0300

    Exercício Finalizado

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,112 @@
 package school.sptech;
 
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.LivroNaoEncontradoException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Biblioteca {
+
+    private String nome;
+    private List<Livro> livros;
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+        livros = new ArrayList<>();
+    }
+
+    public Biblioteca(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro == null || livro.getTitulo() == null || livro.getTitulo().isBlank() || livro.getAutor() == null || livro.getAutor().isBlank() || livro.getDataPublicacao() == null) {
+            throw new ArgumentoInvalidoException("Os argumentos passados estão inválidos");
+        } else {
+            livros.add(livro);
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo) {
+        if (titulo == null || titulo.isEmpty() || titulo.isBlank()) {
+            throw new ArgumentoInvalidoException("Os argumentos passados estão inválidos");
+        } else {
+            Boolean livroRemovido = false;
+            for (int i = 0; i < livros.size(); i++) {
+                Livro livroAtual = livros.get(i);
+                if (livroAtual.getTitulo().equalsIgnoreCase(titulo)) {
+                    livros.remove(livroAtual);
+                    livroRemovido = true;
+                }
+            }
+            if (!livroRemovido) {
+                throw new LivroNaoEncontradoException("O livro não foi encontrado");
+            }
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        if (titulo == null || titulo.isEmpty() || titulo.isBlank()) {
+            throw new ArgumentoInvalidoException("Os argumentos passados estão inválidos");
+        } else {
+            for (int i = 0; i < livros.size(); i++) {
+                Livro livroAtual = livros.get(i);
+                if (livroAtual.getTitulo().equalsIgnoreCase(titulo)) {
+                    return livroAtual;
+
+                }
+            }
+            throw new LivroNaoEncontradoException("O livro não foi encontrado");
+
+        }
+    }
+
+    public Integer contarLivros() {
+        return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        List<Livro> livrosAteAno = new ArrayList<>();
+
+        for (int i = 0; i < livros.size(); i++) {
+            Livro livroAtual = livros.get(i);
+            if (livroAtual.getDataPublicacao().getYear() <= ano ) {
+                livrosAteAno.add(livroAtual);
+            }
+        }
+        return livrosAteAno;
+
+    }
+
+    public List<Livro> retornarTopCincoLivros() {
+        List<Livro> livrosOrdenados = new ArrayList<>();
+        for (Livro livroDaVez : livros) {
+            livrosOrdenados.add(livroDaVez);
+        }
+
+        livrosOrdenados.sort(Comparator.comparingDouble(Livro::calcularMediaAvaliacoes).reversed());
+
+        List<Livro> livrosTopCinco = new ArrayList<>();
+        for (int i = 0; i < livrosOrdenados.size() && i < 5 ; i++) {
+            livrosTopCinco.add(livrosOrdenados.get(i));
+        }
+        return livrosTopCinco;
+    }
+
+
+
 
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,90 @@
 package school.sptech;
 
+import school.sptech.exception.ArgumentoInvalidoException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Livro {
 
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes;
+
+    public Livro(String titulo, String autor, LocalDate dataPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        avaliacoes = new ArrayList<>();
+    }
+
+    public Livro() {
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", dataPublicacao=" + dataPublicacao +
+                ", avaliacoes=" + avaliacoes +
+                '}';
+    }
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas) {
+        if (descricao == null || descricao.isEmpty() || descricao.isBlank() || qtdEstrelas == null || qtdEstrelas < 0 || qtdEstrelas > 5) {
+            throw new ArgumentoInvalidoException("Os argumentos estão inválidos para adicionar avaliação");
+        } else {
+            avaliacoes.add(new Avaliacao(descricao, qtdEstrelas));
+
+        }
+    }
+
+    public Double calcularMediaAvaliacoes() {
+        if (avaliacoes.isEmpty()) {
+            return 0.0;
+        } else {
+            Double somaAvaliacoes = 0.0;
+            for (int i = 0; i < avaliacoes.size(); i++) {
+                Avaliacao avaliacaoAtual = avaliacoes.get(i);
+                somaAvaliacoes += avaliacaoAtual.getQtdEstrelas();
+            }
+            return somaAvaliacoes / avaliacoes.size();
+        }
+    }
 }
